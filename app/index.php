bd421bb363e118610a9eab08fc390c15a00bd647
Fabio Heer <fabio.heer@bluewin.ch>
2016-07-21 16:41:43 +0200

    updated hello

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo <?php echo 'APPUiO'?></h1>
+        <h1>Hallo <?php echo 'Demo'?></h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
